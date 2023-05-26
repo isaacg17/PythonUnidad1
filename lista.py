1eaace2947b3b840b53e317d7ccce6dd60a0e2ec
Christian_Cardenas <ch.cardenasi@duocuc.cl>
2023-05-25 20:17:53 -0400

    operaciones con listas

--- lista.py
+++ lista.py
@@ -1,2 +1,24 @@
-lista_numeros=[1,2,3,4]
-print("esta es la lista:",lista_numeros)
+lista_numeros= [1,2,3]
+print(lista_numeros)
+
+
+
+
+a = [90, "Python", 3.87]
+print(a[-1])
+
+lista = [1, 2]
+lista.append(3)
+print(lista) #[1,2,3]
+
+lista = [1, 2]
+lista.extend([3,4])
+print(lista) #[1, 2, 3, 4]
+
+lista = [1, 3]
+lista.insert(2,"casa")
+print(lista) #[1, 2, 3]
+
+lista  = [1,2,3,4,5,6]
+lista.remove(5)
+print (lista)
