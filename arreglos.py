d7d90cc2d7738f263137c7409a2e6309cb3d2e00
isaac_17 <isa.gomez@duocuc.cl>
2023-05-31 20:58:42 -0400

    Arreglos listas

---
+++ arreglos.py
@@ -0,0 +1,24 @@
+import numpy as np
+lista=[1,2,3,4]
+arr = np.array(lista)
+#print(arr)
+
+#forma 2 directa 
+arr2 = np.array([5,6,7,8])
+#print(arr2)
+
+
+#Funcion con arreglo
+arreglo= np.array([5,6,7,8,9,10,11,12])
+print(arreglo.ndim)
+
+print(arreglo[4:7])
+#Arreglo ndim
+print(arreglo[0:7:3])
+
+print(arreglo[::2])
+
+#Arreglo a partir de un rango
+
+c = [i for i in range(0,101,1)]
+print(c)
